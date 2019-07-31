75b779c4611a721946a2d48a8c4795a52c368cce
Joy Lin <vvlnote@hotmail.com>
2019-07-31 21:08:10 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -9,16 +9,26 @@ class App extends Component {
   render() {
     return(
       <div>
+<<<<<<< HEAD
         <p>{this.state.peopleInSpace.map(person => person.name)}</p>
+=======
+        <p>{this.state.pepoleInSpace.map(person => perons.name)}</p>
+>>>>>>> df0b4747d12c15077879a718536a7a2e3e1104d1
       </div>
     )
   }
 
   componentDidMount() {
     fetch("http://api.open-notify.org/astros.json")
+<<<<<<< HEAD
       .then(response => response.json())
       .then(data => {
         this.setState ({
+=======
+      .then(response => response.json)
+      .tnen(data => {
+        setState ({
+>>>>>>> df0b4747d12c15077879a718536a7a2e3e1104d1
           peolpleInSpace: data.people
         })
       })
